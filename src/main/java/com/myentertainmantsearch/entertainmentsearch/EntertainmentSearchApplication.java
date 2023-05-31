8d407ae85a11aad4064c0fc14072165beac36bc2
Skyforgerr <postnikoviivan@gmail.com>
2023-05-31 13:06:06 +0300

    Initial commit

---
+++ src/main/java/com/myentertainmantsearch/entertainmentsearch/EntertainmentSearchApplication.java
@@ -0,0 +1,13 @@
+package com.myentertainmantsearch.entertainmentsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmentSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmentSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myentertainmantsearch/entertainmentsearch/EntertainmentSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myentertainmantsearch.entertainmentsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntertainmentSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
